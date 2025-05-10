57602cdfdce7e444b4a327cfb25b2ec2a0626c9e
Your Name <aurelsid021@gmail.com>
2025-05-10 11:32:05 +0200

    s

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,52 @@
 import { useState } from 'react';
 import Button from './components/Button';
-import VaultArtifact from "../../artifacts/contracts/Vault.sol/Vault.json";
+// import VaultArtifact from "../../artifacts/contracts/Vault.sol/Vault.json";
 import {ethers} from "ethers";
 
-const wallet = new ethers.Wallet(import.meta.env.VITE_SAPPHIRE_ACCOUNT_PK)
+// const wallet = new ethers.Wallet(import.meta.env.VITE_SAPPHIRE_ACCOUNT_PK)
 
-const provider = new ethers.JsonRpcProvider(import.meta.env.VITE_RPC_URL);
+// const provider = new ethers.JsonRpcProvider(import.meta.env.VITE_RPC_URL);
 
-const signer = wallet.connect(provider);
+// const signer = wallet.connect(provider);
 
 function App() {
 
 	const [updatingContract, setUpdatingContract] = useState(false);
 
-	const updateContract = async () => {
+	// const updateContract = async () => {
 
-		try {
-			const contract = new ethers.Contract(
-				import.meta.env.VITE_CONTRACT_ADDRESS,
-				VaultArtifact.abi,
-				signer
-			);
+	// 	try {
+	// 		const contract = new ethers.Contract(
+	// 			import.meta.env.VITE_CONTRACT_ADDRESS,
+	// 			VaultArtifact.abi,
+	// 			signer
+	// 		);
 
-			console.log("Connected to Vault contract:", contract);
+	// 		console.log("Connected to Vault contract:", contract);
 
 
-			const tx = await contract.storeBetData(ethers.parseEther("0.01"), 3);
+	// 		const tx = await contract.storeBetData(ethers.parseEther("0.01"), 3);
 			
-			const receipt = await tx.wait();
+	// 		const receipt = await tx.wait();
 
-			console.log("Transaction confirmed:", receipt);
+	// 		console.log("Transaction confirmed:", receipt);
 
-			const res = await contract.getBets(3);
+	// 		const res = await contract.getBets(3);
 			
-			res.forEach(bet => {
-				console.log("Bet Amount:", ethers.formatEther(bet.amount));
-				console.log("Better Address:", bet.better);
-			});
+	// 		res.forEach(bet => {
+	// 			console.log("Bet Amount:", ethers.formatEther(bet.amount));
+	// 			console.log("Better Address:", bet.better);
+	// 		});
 		
-		} catch (error) {
-			console.error("Error connecting to the contract", error);
-		}
+	// 	} catch (error) {
+	// 		console.error("Error connecting to the contract", error);
+	// 	}
 
-	}
+	// }
 
 	const handleClick = () => {
 		setUpdatingContract(true);
-		updateContract();
+		// updateContract();
 		setUpdatingContract(false);
 	}
 
